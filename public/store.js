abdba15cb05b0bbcbbe734b944b48f99c9f7909a
Victor Barraza <victor.barrazamerino@medieinstitutet.se>
2021-09-23 15:57:12 +0200

    ändringar

---
+++ public/store.js
@@ -0,0 +1,8 @@
+var removeCartItemButtons = document.getElementsByClassName('removePurchase')
+console.log(removeCartItemButtons)
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event) {
+       var buttonClicked = event.target
+    })
+}
