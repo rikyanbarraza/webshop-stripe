72929a2729d0d856bddf3e641ca3513fab0af14b
Andreas Palm Österberg <72131407+pop-in-se@users.noreply.github.com>
2021-09-30 21:25:56 +0200

    cartnumbers, totalamount

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
 const purchaseBtn = document.getElementById('makePurchase');
 const toCartBtn = document.getElementById('toCart');
 
-
 let stripe = Stripe('pk_test_51Jc7a0L3xesmMHJgbAPWLEn3t20phRDibRXtF4h6cBfYbSV6ZB9dI3VD3hjWL7uTVRYdH0Zz00n14HIeRLmSwv6i00dsOw4Jj9')
 
 //Produktlista
@@ -31,56 +30,92 @@ const productList =
             },
             unit_amount: 1000
         }
+    }, 
+"Soda": {
+description: 'Läskande dryck',
+
+price_data: {
+currency: 'sek',
+    product_data: {
+        name: "Soda",   
+        images: ['http://localhost:3000/Content/soda.jpg'],  
     },
-    "Cookie": {
-        description: 'Kaka',
-    
-        price_data: {
-            currency: 'sek',
-                product_data: {
-                    name: "Cookie",   
-                    images: ['http://localhost:3000/Content/cookie.jpg'],  
-                },
-                unit_amount: 1000
-            }
+    unit_amount: 1000
+}
+},  
+"Energidryck": {
+description: 'Läskande energidryck',
+
+price_data: {
+    currency: 'sek',
+        product_data: {
+            name: "Energidryck",   
+            images: ['http://localhost:3000/Content/energidryck.jpg'],  
         },
-    "Cube": {
-        description: 'Kub',
-    
-        price_data: {
-            currency: 'sek',
-                product_data: {
-                    name: "Cube",   
-                    images: ['http://localhost:3000/Content/cube.jpg'],  
-                },
-                unit_amount: 1000
-            }
-        },    
-    "Mask": {
-        description: 'Ansiktsmask',
-    
-        price_data: {
-            currency: 'sek',
-                product_data: {
-                    name: "Mask",   
-                    images: ['http://localhost:3000/Content/mask.jpg'],  
-                },
-                unit_amount: 1000
-            }
-        },  
-        "Soda": {
-            description: 'Läskande dryck',
-        
-            price_data: {
-                currency: 'sek',
-                    product_data: {
-                        name: "Soda",   
-                        images: ['http://localhost:3000/Content/soda.jpg'],  
-                    },
-                    unit_amount: 1000
-                }
-            },   
-    
+        unit_amount: 1000
+    }
+},
+"Cube": {
+    description: 'Kub',
+
+    price_data: {
+        currency: 'sek',
+            product_data: {
+                name: "Cube",   
+                images: ['http://localhost:3000/Content/cube.jpg'],  
+            },
+            unit_amount: 1000
+        }
+    },    
+"Mask": {
+    description: 'Ansiktsmask',
+
+    price_data: {
+        currency: 'sek',
+            product_data: {
+                name: "Mask",   
+                images: ['http://localhost:3000/Content/mask.jpg'],  
+            },
+            unit_amount: 1000
+        }
+    }, 
+"Cookie": {
+    description: 'Kaka',
+
+    price_data: {
+        currency: 'sek',
+            product_data: {
+                name: "Cookie",   
+                images: ['http://localhost:3000/Content/cookie.jpg'],  
+            },
+            unit_amount: 1000
+        }
+    },
+"Espresso": {
+    description: 'En kopp Espresso',
+
+    price_data: {
+        currency: 'sek',
+            product_data: {
+                name: "Espresso",   
+                images: ['http://localhost:3000/Content/espresso.jpg'],  
+            },
+            unit_amount: 1000
+        }
+    },  
+"Sandwich": {
+    description: 'En kopp Espresso',
+
+    price_data: {
+        currency: 'sek',
+            product_data: {
+                name: "Sandwich",   
+                images: ['http://localhost:3000/Content/sandwich.jpg'],  
+            },
+            unit_amount: 1000
+        }
+    }, 
+                        
 }
 
 //Kundkorg
@@ -92,22 +127,78 @@ const addProduct = async (productKey) => {
     
     if (!product) {
         throw new Error("Product does not exist")
+        
     }
     cart[productKey] = product
     cart[productKey].quantity = cart[productKey].quantity || 0;
     cart[productKey].quantity++;
-    console.log({ cart, line_items: Object.values(cart)})
+    console.log({ line_items: Object.values(cart)})
 
-    localStorage.setItem("cart", JSON.stringify(cart));
-    console.log(cart)
-
-    cartQuantity = cart[productKey].quantity
+    /* localStorage.setItem("cart", JSON.stringify(cart)); */
     
-    if (cartQuantity) {
-        document.getElementById('cartQuantity').innerText = cartQuantity;
-    }
-}
 
+     
+    // Varukorg totalt antal varor - totalt pris
+    
+    let totalPrice = 0
+    let totalCartQuantity = 0
+
+    Object.entries(cart).map((products) => {
+        totalPrice = totalPrice + products[1].price_data.unit_amount * products[1].quantity;
+        totalCartQuantity = totalCartQuantity + products[1].quantity
+        document.getElementById('cartQuantity').innerText = totalCartQuantity;
+                
+    })
+
+    // Rendera ut varor i varukorgsmodal
+
+    document.getElementById("toCart").style.display = 'block';
+    document.getElementById("noProducts").innerText = ""
+
+    productCartUl = document.getElementById("productInCartUl")
+    productCartLi = document.createElement('li')
+    productCartLi.setAttribute("class", "list-group-item d-flex justify-content-between lh-sm");
+    productInCart = document.createElement('div')
+    productInCartTitle = document.createElement('h6')
+    productInCartTitle.setAttribute("class", "my-0");
+    productInCartTitle.innerText = product.price_data.product_data.name
+  
+
+    productCartUl.appendChild(productCartLi)
+    productCartLi.appendChild(productInCart)
+    productInCart.appendChild(productInCartTitle)
+    
+    productInCartDescription = document.createElement('small')
+    productInCartDescription.setAttribute("class", "text-muted");
+    productInCartDescription.innerText = product.description
+    productInCart.appendChild(productInCartDescription)
+    
+  
+    productInCartPriceText = document.createElement('span')
+    productInCartPriceText.setAttribute("class", "text-muted"); 
+    productInCartPriceText.innerText = product.price_data.unit_amount /100 + " kr"
+    productCartLi.appendChild(productInCartPriceText)
+
+   
+    totalPriceLi = document.getElementById("totalPriceLi")
+    totalPriceLi.setAttribute("class", "list-group-item d-flex justify-content-between");
+    totalPriceText = document.getElementById("totalPriceText")
+    totalPriceText.innerText = "Totalt"
+
+
+
+    productInCartTotalPriceText = document.getElementById("InCartTotalPriceText")
+    productInCartTotalPriceText.setAttribute("class", "text-muted");
+    productInCartTotalPriceText.innerText = totalPrice /100 + " kr"
+
+    
+    totalPriceLi.appendChild(productInCartTotalPriceText)
+
+
+
+
+   
+}
 
 
 //Rendera ut produkter 
@@ -194,4 +285,5 @@ catch (err) {
 }
 
 toCartBtn.addEventListener('click', () => checkout())
+
 
